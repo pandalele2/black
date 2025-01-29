86ad0af67e2a46f45ca51ba15d9b0c7ba7221e3d
pandalele2 <pandahulk57@gmail.com>
2025-01-29 09:23:10 +0800

    sty.js

--- sty.js
+++ sty.js
@@ -3314,7 +3314,7 @@
               "poster": "https://ecentral.my/wp-content/uploads/2024/12/curang-tanpa-niat-1.webp",
               "plot": "",
               "director": ["Eyra Rahman"], 
-              "cast": [" Shukri Yahya","Mira Filzah","Sharifah Sakinah"],
+              "cast": [" Shukri Yahya,  Mira Filza,  Sharifah Sakinah"],
               "year": "2024"
              },
     "seasons": [
@@ -3325,7 +3325,7 @@
                     "plot": "Kisah Pergelutan Seorang Isteri Bernama Elina dalam Mengharungi Ketidaksetiaan dan Kecurangan suaminya, Eshaal di tempat kerja.",
                     "backdrop": "https://ecentral.my/wp-content/uploads/2024/12/curang-tanpa-niat-1.webp", 
                     "director": ["Jamal Khan"], 
-                    "cast": ["Harissa Adlynn Halim","Redza Rosli","Kael Noah"],
+                    "cast": [" Harissa Adlynn Halim,  Redza Rosli,  Kael Noah"],
                     "year": "2024"
                     },
            "episodes": [       
@@ -3338,7 +3338,7 @@
                             "plot": "Kisah Pergelutan Seorang Isteri Bernama Elina dalam Mengharungi Ketidaksetiaan dan Kecurangan suaminya, Eshaal di tempat kerja.",
                             "backdrop": "https://ecentral.my/wp-content/uploads/2024/12/curang-tanpa-niat-1.webp", 
                             "director": ["Eyra Rahman"], 
-                            "cast": ["Shukri Yahya","Mira Filzah","Sharifah Sakinah"],
+                            "cast": [" Shukri Yahya,  Mira Filzah,  Sharifah Sakinah"],
                             "year": "2024"
                            },
                     "video": "https://vodejitp-asset-playback-b.astro.com.my/v4/default/dash-wv/vode/a271c107-ae98-5ed9-aa81-491b804153b4/default_primary.mpd", 
@@ -3354,7 +3354,7 @@
                             "plot": "Kisah Pergelutan Seorang Isteri Bernama Elina dalam Mengharungi Ketidaksetiaan dan Kecurangan suaminya, Eshaal di tempat kerja.",
                             "backdrop": "https://ecentral.my/wp-content/uploads/2024/12/curang-tanpa-niat-1.webp", 
                             "director": ["Eyra Rahman"], 
-                            "cast": ["Shukri Yahya","Mira Filzah","Sharifah Sakinah"],
+                            "cast": [" Shukri Yahya,  Mira Filzah,  Sharifah Sakinah"],
                             "year": "2024"
                            },
                     "video": "https://vodejitp-asset-playback-b.astro.com.my/v4/default/dash-wv/vode/658a52a6-4b33-5ae9-8ada-9fb8a823b2e7/default_primary.mpd", 
