d7b482d9feaf2cbd9d5b5cd7ff0a5dde85bf57cc
pandalele2 <pandahulk57@gmail.com>
2025-01-29 16:24:18 +0800

    Update sty.js

--- sty.js
+++ sty.js
@@ -3339,7 +3339,7 @@
                             "cast": [" Shukri Yahya,  Mira Filzah,  Sharifah Sakinah"],
                             "year": "2024"
                            },
-                    "video": "https://vodejitp-asset-playback-b.astro.com.my/v4/default/dash-wv/vode/a271c107-ae98-5ed9-aa81-491b804153b4/default_primary.mpd", 
+                    "video": "https://dikv9338-e051.dikv9338.workers.dev/0:/ctn1.mp4", 
                     "drm": "clearkey", 
                     "drmkey": ""
                  },
@@ -3355,9 +3355,9 @@
                             "cast": [" Shukri Yahya,  Mira Filzah,  Sharifah Sakinah"],
                             "year": "2024"
                            },
-                    "video": "https://vodejitp-asset-playback-b.astro.com.my/v4/default/dash-wv/vode/658a52a6-4b33-5ae9-8ada-9fb8a823b2e7/default_primary.mpd", 
+                    "video": "https://dikv9338-e051.dikv9338.workers.dev/0:/ctn2.mp4", 
                     "drm": "clearkey", 
-                    "drmkey": "60fe459d5d8563dde7bdb103800b3210:8aa1a14c498fec7680092fdf26d3c8a3"
+                    "drmkey": ""
               }
             ]
         }      
